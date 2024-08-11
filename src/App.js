f3eeb20f203e4d236f2b39941bdf06892c6eb602
WebDev2105 <choudareddy9842@gmail.com>
2024-08-11 18:15:11 +0530

    Registration & Login & Logout V1.2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
+import React from "react";
+import "./GlobalCSS.css";
+import { Routes, Route } from "react-router-dom";
 import { Registration } from "./Components/Registration";
+import { Home } from "./Components/Home";
+import { Header } from "./Model/Header";
+import { Menu } from "./Model/Menu";
+import { Login } from "./Components/Login";
+import { Logout } from "./Components/LogOut";
+
 function App() {
+  const [isAdminLoggedIn, setIsAdminLoggedIn] = React.useState(false);
+
+  React.useEffect(() => {
+    // Check localStorage for the login status on initial load
+    const loggedInStatus = localStorage.getItem("isAdminLoggedIn") === "false";
+    setIsAdminLoggedIn(loggedInStatus);
+  }, []);
+
+  const handleLogin = () => {
+    setIsAdminLoggedIn(true);
+    localStorage.setItem("isAdminLoggedIn", "true");
+  };
+  const handleLogout = () => {
+    setIsAdminLoggedIn(false);
+    localStorage.removeItem("isAdminLoggedIn");
+  };
   return (
-    <div>
-      <Registration />
-    </div>
+    <>
+      <Header />
+      <Menu isAdminLoggedIn={isAdminLoggedIn} />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/registration" element={<Registration />} />
+        <Route path="/login" element={<Login onLogin={handleLogin} />} />
+        <Route path="/home" element={<Logout onLogout={handleLogout} />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/Model/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Header.css";
+export const Header = () => {
+  return (
+    <div className="header-body">
+      <h1 className="header">Welcome to Madhuri men's Pg and Hostals</h1>
+    </div>
+  );
+};
